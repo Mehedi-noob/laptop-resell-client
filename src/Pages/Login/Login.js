3f7c00596bd629e639813416411cdd9be7987de3
Mehedi Hasan <you@example.com>
2022-11-24 14:03:09 +0600

    login and register page completed

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,65 @@
+import React, { useContext, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
+
+const Login = () => {
+    const [error, setError] = useState('');
+    const { signIn, setLoading } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || '/';
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+                setError('');
+                if (user.uid) {
+                    navigate(from, { replace: true });
+                    console.log("navigate is working")
+                }
+            })
+            .catch(error => {
+                console.error(error)
+                setError(error.message);
+            })
+            .finally(() => {
+                setLoading(false);
+            })
+    }
+    return (
+        <div className='flex justify-center'>
+            <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-900 dark:text-gray-100">
+                <h1 className="text-2xl font-bold text-center">Login</h1>
+                <form onSubmit={handleSubmit} action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+                    <div className="space-y-1 text-sm">
+                        <label className="block dark:text-gray-400">Email</label>
+                        <input type="text" name="email" id="email" placeholder="email" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                    </div>
+                    <div className="space-y-1 text-sm">
+                        <label className="block dark:text-gray-400">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        <div className="flex justify-end text-xs dark:text-gray-400">
+                            <a rel="noopener noreferrer" href="#">Forgot Password?</a>
+                        </div>
+                    </div>
+                    <div className="form-control mt-6">
+                        <input className="btn btn-primary" type="submit" value="Login" />
+                    </div>
+                </form>
+                <p className="text-xs text-center sm:px-6 dark:text-gray-400">Don't have an account?
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
