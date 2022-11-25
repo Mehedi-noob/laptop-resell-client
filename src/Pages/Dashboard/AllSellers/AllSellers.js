4dc63225a6aa04b3e629616e069370ce3f274fa9
Mehedi Hasan <you@example.com>
2022-11-25 23:38:56 +0600

    all sellers trial page created

---
+++ src/Pages/Dashboard/AllSellers/AllSellers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllSellers = () => {
+    return (
+        <div>
+            <h1>this is all sellers page.</h1>
+        </div>
+    );
+};
+
+export default AllSellers;
