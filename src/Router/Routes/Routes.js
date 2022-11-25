e2ff4038dba3336dcea188b41d4dc51e8711f357
Mehedi Hasan <you@example.com>
2022-11-25 23:43:09 +0600

    route element iteration solved

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -2,9 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Main/Main";
 import CategoryProducts from "../../Pages/CategoryProducts/CategoryProducts";
 import AddProduct from "../../Pages/Dashboard/AddProduct/AddProduct";
+import AllBuyers from "../../Pages/Dashboard/AllBuyers/AllBuyers";
+import AllSellers from "../../Pages/Dashboard/AllSellers/AllSellers";
 import Dashboard from "../../Pages/Dashboard/Dashboard";
 import DashHome from "../../Pages/Dashboard/DashHome/DashHome";
 import MyOrders from "../../Pages/Dashboard/MyOrders/MyOrders";
+import MyProduct from "../../Pages/Dashboard/MyProduct/MyProduct";
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Register from "../../Pages/Register/Register";
@@ -62,21 +65,21 @@ const router = createBrowserRouter([
                 // loader: ({ params }) => {
                 //     return fetch(`http://localhost:5000/booking/${params.email}`);
                 // },
-                element: <AddProduct></AddProduct>
-            }
+                element: <AllSellers></AllSellers>
+            },
             {
                 path: '/dashboard/allbuyers',
                 // loader: ({ params }) => {
                 //     return fetch(`http://localhost:5000/booking/${params.email}`);
                 // },
-                element: <AddProduct></AddProduct>
-            }
+                element: <AllBuyers></AllBuyers>
+            },
             {
                 path: '/dashboard/myproduct',
                 // loader: ({ params }) => {
                 //     return fetch(`http://localhost:5000/booking/${params.email}`);
                 // },
-                element: <AddProduct></AddProduct>
+                element: <MyProduct></MyProduct>
             }
         ]
     }
