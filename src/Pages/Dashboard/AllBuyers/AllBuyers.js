561a00dd099551efddffa36756b9bec69ff08654
Mehedi Hasan <you@example.com>
2022-11-25 23:40:09 +0600

    all buyers trial page created

---
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllBuyers = () => {
+    return (
+        <div>
+            <h1>this is my all buyers page.</h1>
+        </div>
+    );
+};
+
+export default AllBuyers;
